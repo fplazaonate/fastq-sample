ee33a5b38b7f192d7ba80b51fd4b59fc45346890
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2016-05-11 13:24:58 +0200

    Bad copy/paste

--- Settings.cc
+++ Settings.cc
@@ -32,7 +32,8 @@ Settings Settings::get_settings(int argc, char* argv[])
     po::options_description opts_desc( "Random sampling of reads from FASTQ files");
 
     opts_desc.add_options()
-/bin/bash: qa : commande introuvable
+        ("help,h", "print this help message")
+        ("fastq-files,f", po::value<std::vector<std::string> >(&settings.fastq_files)->multitoken(), "input FASTQ files")
         ("output-file,o", po::value<std::string>(&settings.output_file), "output FASTQ file")
         ("target-num-reads,r", po::value<size_t>(&settings.target_num_reads)->default_value(0), "number of reads to sample")
         ("target-num-bases,b", po::value<size_t>(&settings.target_num_bases)->default_value(0), "number of bases to sample")
